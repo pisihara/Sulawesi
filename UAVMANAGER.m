c83f4764109a32d827392e710f1c385133b7a44a
Michaela Flitsch <44982383+michaelaflitsch@users.noreply.github.com>
2018-12-03 13:15:20 -0600

    Add files via upload

--- UAVMANAGER.m
+++ UAVMANAGER.m
@@ -10,19 +10,21 @@ classdef UAVMANAGER < handle
         % (i,4)  = destination zone
         %        base= zone 0  parked = -1
         % (i,5) = speed in km/hr
+       
     end
     
     
     methods
         % Constructor 
         
-        function obj = UAVMANAGER(x,y,batterylife,zone,requestID,speed)
+        function obj = UAVMANAGER(x,y,batterylife,zone,requestID,speed,park)
           obj.UAVlog(:,1) = x; % initial x coordinate of UAVs
           obj.UAVlog(:,2) = y; % initial y coordinates of UAVs
           obj.UAVlog(:,3) = batterylife; % battery life of UAVs (minutes)
           obj.UAVlog(:,4) = zone; % zone of destination of UAVs
           obj.UAVlog(:,5) = requestID; % requests assigned to UAVs
           obj.UAVlog(:,6) = speed; % speed in km/hr of UAVs
+          obj.UAVlog(:,7) = park; %remaining time for UAV to be parked
         end
         
         function [obj,RM]= assignUAVs(obj,REQUESTMANAGER,reqid,fleetsize)
@@ -44,11 +46,14 @@ classdef UAVMANAGER < handle
             RM=REQUESTMANAGER;
             for i=1:fleetsize                                    
                 if  obj.UAVlog(i,5)~= 0  % UAV has an active request
+                  
                   x1= obj.UAVlog(i,1);  %x-coord of UAV
                   y1= obj.UAVlog(i,2); % y-coord of UAV
                   x2=RM.requestlog(obj.UAVlog(i,5),4); %x-coord of drop-off
                   y2=RM.requestlog(obj.UAVlog(i,5),5); % y-ccord of drop off
                   d=((x2-x1)^2+(y2-y1)^2)^.5;
+                  d2=((1050-x2)^2+(1700-y2)^2)^.5;
+                  d3=d+d2;
                   dx = (100/1.8204)*(1/60)*(x2-x1)*obj.UAVlog(i,6)/d;
                   dy = (100/1.8204)*(1/60)*(y2-y1)*obj.UAVlog(i,6)/d;
                   if sqrt(dx^2+dy^2) < d
@@ -57,7 +62,14 @@ classdef UAVMANAGER < handle
                   else 
                       obj.UAVlog(i,1) = x2;
                       obj.UAVlog(i,2) = y2;
+                      obj.UAVlog(i,7) = obj.UAVlog(i,7)-1;
+                      if obj.UAVlog(i,7) == 0
+                          obj.UAVlog(i,5) = 0;
+                          obj.UAVlog(i,7) = 5;
+                      end
+                      
                   end
+                  obj.UAVlog(i,3) = obj.UAVlog(i,3)-1;
                 end
             end
         end
